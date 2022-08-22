e0c72cb378937fb9eacb6cdbd0f490572cf4be7d
fernandocristiano <75613223+fernandocristiano@users.noreply.github.com>
2022-08-22 15:22:39 +0100

    Font Added

---
+++ app/src/main/java/uk/fernando/memory/theme/Font.kt
@@ -0,0 +1,12 @@
+package uk.fernando.memory.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import uk.fernando.memory.R
+
+val solwayFamily = FontFamily(
+    Font(R.font.solway_regular, FontWeight.Normal),
+    Font(R.font.solway_bold, FontWeight.Bold),
+    Font(R.font.solway_medium, FontWeight.Medium),
+)
