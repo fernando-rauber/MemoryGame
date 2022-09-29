b5848a87516bda9bbd9d8ed7046b7844a062852c
fernandocristiano <75613223+fernandocristiano@users.noreply.github.com>
2022-09-29 09:15:59 +0100

    Added single click on modifier clickable

--- app/src/main/java/uk/fernando/memory/BaseApplication.kt
+++ app/src/main/java/uk/fernando/memory/BaseApplication.kt
@@ -11,7 +11,7 @@ class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        //MyAdvertising.setDeviceID(listOf("1B8A325EEFF8BEF2134994B7A47F8F19"))
+//        MyAdvertising.setDeviceID(listOf("1B8A325EEFF8BEF2134994B7A47F8F19"))
         MyAdvertising.initialize(this)
 
         startKoin {
